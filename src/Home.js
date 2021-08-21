b65b4c02684ce748c15728b6fe52abebe0ba9b6f
fncmacaroon <80917836+fncmacaroon@users.noreply.github.com>
2021-08-20 22:47:37 -0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
       <div>
         <h2>Best Buy</h2>
         <p><StyledLink>Best Buy is a company that is well known to sell electronics and have a group of people that can help you understand and even repair these electronics for you.<br></br> 
-          Our range of products go from Televisions, Refrigerators, Washers to phones and gaming consoles. Testing Commit 2</StyledLink></p>
+          Our range of products go from Televisions, Refrigerators, Washers to phones and gaming consoles. Commit 1</StyledLink></p>
  
         <p><StyledLink>We offer curbside pickup within in hour of placing an order</StyledLink></p>
         <p><StyledLink>Free next-day delivery</StyledLink></p>
